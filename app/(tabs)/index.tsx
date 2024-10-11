9e7b39c92d2bae166a48de42d23edabf30d53590
naveenmathramkott <naveenmathramkott@gmail.com>
2024-10-11 19:39:50 +0530

    tailwindcss added readme updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,12 @@
-import { Image, StyleSheet, Platform } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      // headerImage={
-      //   <Image
-      //     source={require("@/assets/images/partial-react-logo.png")}
-      //     style={styles.reactLogo}
-      //   />
-      // }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Rydon App</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <View>
+        <Text className="text-black">Ryvon</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
