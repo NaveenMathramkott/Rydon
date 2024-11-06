d60cd38dc5ad69144c11e1d5337be7e49236e207
naveenmathramkott <naveenmathramkott@gmail.com>
2024-11-06 18:45:30 +0530

    welcome screens and clerk authentication is added

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Home = () => {
+  return <Text className="text-white">APP HOME </Text>;
+};
+
+export default Home;
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Profile = () => {
+  return <Text className="text-white">Profile page </Text>;
+};
+
+export default Profile;
